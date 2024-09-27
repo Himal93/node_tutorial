dc32b60e9b273235370cb5f8f7b3087f90f24ab3
Himal93 <himalranamgr@gmail.com>
2024-09-27 18:09:54 +0545

    update

--- auth.js
+++ auth.js
@@ -7,12 +7,11 @@ const Person = require('./models/Person');
 passport.use(new LocalStrategy(async (username, password, done) =>{
     //authentication logic here
     try{
-    //   console.log('receive credentials:', username, password);
       const user = await Person.findOne({username});
       if(!user)
         return done(null, false, {message: "Incorrect username"});
   
-      const isPasswordMatch = user.password === password ? true : false;
+      const isPasswordMatch = await user.comparePassword(password);
       if(isPasswordMatch){
         return done(null, user);
       }else{
--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // define the person schema
 const personSchema = new mongoose.Schema({
@@ -39,6 +40,38 @@ const personSchema = new mongoose.Schema({
     }
 });
 
+personSchema.pre('save', async function(next){
+    const person = this;
+
+    // hash th password only if it has been modified(or new)
+    if(!person.isModified('password')) return next();
+
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash passowrd
+        const hashPassword = await bcrypt.hash(person.password, salt);
+        // override the plain password with hashed one
+        person.password = hashPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+});
+
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // use bcrypt to compare provided password with hashed password 
+        //concept is compare function extract salt from storedhashPassword and uses it to hash the entered password the compares it if they match or not
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
+
 // Create Person model
 const Person = mongoose.model('Person', personSchema);
+
 module.exports = Person;
